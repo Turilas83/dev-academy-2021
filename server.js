254bf3818090bf96057775fa0eb53435f3f9d84c
Mikko Kaasinen <turilas@Mikko-MacBook-Pro.local>
2020-12-28 15:43:29 +0200

    Making backend to work as proper API returning JSON

--- server.js
+++ server.js
@@ -5,33 +5,23 @@ const app = express();
 
 var names = require(__dirname + "/names.json")
 
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/most-popular", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res, next) => {
+    return res.status(200).json({ error: false, message: "OK" })
+});
+
+// Endpoint to get names ordered by most popular
+app.get("/names/most-popular", (req, res, next) => {
   // sort by value
   names.names.sort(function (a, b) {
     return( b.amount - a.amount);
   });
-  console.log(names);
-  CountOfNames()
+  return res.status(200).json(names);
 });
 
-app.post("/search", function(req, res){
-  const nameToSearch = req.body.nameSearch;
-  for (var key of names.names) {
-    if (key.name === nameToSearch) {
-      console.log(key.amount);
-      res.write("<p>Found " + key.amount + " workers named " + nameToSearch + "</p>");
-      res.send();
-    }
-  }
-
-});
-
-app.get("/alphabetical-order", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+// Endpoint to get names ordered by alphabetcal order
+app.get("/names/alphabetical-order", (req, res, next) => {
   // sort by name
   names.names.sort(function(a, b) {
     var nameA = a.name.toUpperCase(); // ignore upper and lowercase
@@ -45,20 +35,31 @@ app.get("/alphabetical-order", function(req, res){
     // names must be equal
     return 0;
   });
-  console.log(names);
-  CountOfNames();
+  return res.status(200).json(names);
 });
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000");
-});
-
-function CountOfNames() {
+// Endpoint to get total count of names
+app.get("/names/count", (req, res, next) => {
   var count = 0;
   for (var key of names.names) {
     if (key.amount) {
       count = count + key.amount;
     }
   }
-  console.log(count);
-};
+  return res.status(200).json({ error: false, count: count });
+});
+
+// Endpoint to get name with given search parameter
+app.get('/names/search/:name', (req, res, next) => {
+  let name = req.params.name;
+  name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  for (var key of names.names) {
+    if (key.name === name) {
+      return res.status(200).json({ name: key.name, amount: key.amount });
+    }
+  }
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
