d7a99bcb770489a2626bb5a8533b6b89b259a79e
Mikko Kaasinen <turilas@Mikko-MacBook-Pro.local>
2021-01-02 18:04:49 +0200

    try to fix build

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
-router.get('/', controllers.defRoute);
+//router.get('/', controllers.defRoute);
 router.get('/count', controllers.nameCount);
 router.get('/most-popular', controllers.mostPopular);
 router.get('/alphabetical-order', controllers.alphabeticalOrder);
@@ -10,7 +10,7 @@ router.get('/search', controllers.search);
 
 module.exports = router;
 
-module.exports.defRoute = defRoute;
+//module.exports.defRoute = defRoute;
 module.exports.mostPopular = mostPopular;
 module.exports.alphabeticalOrder = alphabeticalOrder;
 module.exports.nameCount = nameCount;
