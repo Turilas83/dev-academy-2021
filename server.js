7d6c6a61f08231072d474b31174cd41fc8951ffd
Mikko Kaasinen <turilas@Mikko-MacBook-Pro.local>
2021-01-02 16:24:34 +0200

    Change structure

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ let employees = require(__dirname + "/names.json")
 app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname+"../client/build/index.html"));
+  res.sendFile(path.join(__dirname+"client/build/index.html"));
 });
 
 // Default endpoint, returns response code 200 if up and running
